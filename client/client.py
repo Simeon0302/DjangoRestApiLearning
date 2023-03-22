8d8223688628c7dc874fd40e699f7a5d462ac1cb
Simeon_32 <simeon32.dev@gmail.com>
2023-03-22 17:41:06 +0200

    Added the RetrieveAPIView for the products. Now when the user goes to ../api/products/x/ and in the place of x they put a number they will receive the x product. The client is modified to show the first product

--- client/client.py
+++ client/client.py
@@ -1,9 +1,10 @@
 import requests
 
-endpoint = "http://localhost:8000/api/"
+endpoint_GET = "http://localhost:8000/api/products/1/"
+endpoint_POST = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
-post_response = requests.post(endpoint, json={'title': 'Hello World', 'content': 'Hello World content', 'price': 12})
+get_response = requests.get(endpoint_GET)
+post_response = requests.post(endpoint_POST, json={'title': 'Hello World5', 'content': 'Hello World content', 'price': 12})
 
 print(get_response.json())
 print(post_response.json())
